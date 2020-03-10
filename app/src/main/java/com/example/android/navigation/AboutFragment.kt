dc9ca6d67379393898361c447754681f586e66ea
SammyWin <kingstonru@yandex.ru>
2020-03-11 00:21:09 +0300

    Cloned the starter code from repo

--- app/src/main/java/com/example/android/navigation/AboutFragment.kt
+++ app/src/main/java/com/example/android/navigation/AboutFragment.kt
@@ -22,7 +22,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class AboutFragment : Fragment() {
+class  AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
