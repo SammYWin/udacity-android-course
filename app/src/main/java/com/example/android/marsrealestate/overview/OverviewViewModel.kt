39346fa715715f59e6997af46e522a9be715a9bf
SammyWin <kingstonru@yandex.ru>
2020-04-19 00:42:33 +0300

    lesson_8 started, created retrofit service, requested a JSON file using coroutines and displaying image with Glide

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,18 +20,26 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-/**
- * The [ViewModel] that is attached to the [OverviewFragment].
- */
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData String that stores the status of the most recent request
-    private val _response = MutableLiveData<String>()
 
-    // The external immutable LiveData for the request status String
-    val response: LiveData<String>
-        get() = _response
+    private val _status = MutableLiveData<String>()
+    val status: LiveData<String>
+        get() = _status
+
+    private val _property = MutableLiveData<MarsProperty>()
+    val property: LiveData<MarsProperty>
+        get() = _property
+
+    private var viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -40,10 +48,23 @@ class OverviewViewModel : ViewModel() {
         getMarsRealEstateProperties()
     }
 
-    /**
-     * Sets the value of the status LiveData to the Mars API status.
-     */
     private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
+        uiScope.launch {
+            val getPropertiesDeferred = MarsApi.retrofitService.getProperties()
+            try {
+                val listResult = getPropertiesDeferred.await()
+                if (listResult.size > 0){
+                    _property.value = listResult[0]
+                }
+            } catch (t: Throwable){
+                _status.value = "Request failed: ${t.message}"
+            }
+
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
